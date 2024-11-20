4ec3655ed27d6e2d093446ef0130faf53db67c43
Christiaan Peppenster <christiaan@ngtech.nl>
2024-11-20 13:11:51 +0100

    Different modem lib

--- build.rs
+++ build.rs
@@ -99,7 +99,7 @@ fn main() {
 
 	#[cfg(feature = "nrf9120")]
 	let libmodem_original_path =
-		Path::new(&nrfxlib_path).join("nrf_modem/lib/cellular/nrf9120/hard-float/libmodem.a");
+		Path::new(&nrfxlib_path).join("nrf_modem/lib/dect_phy/nrf9120/hard-float/libmodem.a");
 
 	let libmodem_changed_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("libmodem.a");
 
